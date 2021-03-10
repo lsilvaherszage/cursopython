27fbf1f39daed2ccb71908fc687dac8ef330121d
Gonzalo Zabala <gzabala@gmail.com>
2021-03-09 21:26:23 -0300

    Actualización

--- Ejemplosbasicos/UsoCSV.py
+++ Ejemplosbasicos/UsoCSV.py
@@ -1,7 +1,11 @@
-import csv
-with open("Contenido\Ejemplosbasicos\covid.csv") as archi:
-    import csv
-    lector = csv.DictReader(archi)
-    for fila in lector:
-        print(fila["iso_code"], fila["continent"])
+# pip install -U pandasql
 
+import pandas as pd
+import pandasql as psql
+
+data = pd.read_csv("Contenido\Ejemplosbasicos\owid-covid-data.csv",header=0)
+pais=input("Ingrese país: ")
+anio=input("Ingrese año (4 dig): ")
+mes=input("Ingrese mes (2 dig): ")
+resu=psql.sqldf("Select strftime('%m', date) as mes, sum(new_cases) as total from data where location='{}' and strftime('%Y', date)='{}' GROUP BY strftime('%m', date)".format(pais, anio))
+print(resu)
